ea4e65ff4c78b49ece48f9d237018c18cc6215d0
PedroMeloBitencourt <89912712+pedrinv@users.noreply.github.com>
2022-07-17 11:38:33 -0300

    Create Pagavel.java

---
+++ Pagamento/Pagavel.java
@@ -0,0 +1,10 @@
+/*
+Nome: Pedro Vitor Melo Bitencourt
+Matrícula: 20213001540
+*/
+package principal;
+public abstract class Pagavel {
+    
+    public abstract double getValorAPagar(int diaPagamento, int mesPagamento);
+    
+}
