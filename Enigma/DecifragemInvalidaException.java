26a7c49edf943453d564c67ed0b053dd860886e4
PedroMeloBitencourt <89912712+pedrinv@users.noreply.github.com>
2022-07-17 11:43:15 -0300

    Create DecifragemInvalidaException.java

---
+++ Enigma/DecifragemInvalidaException.java
@@ -0,0 +1,12 @@
+/*
+Nome: Pedro Vitor Melo Bitencourt
+Matrícula: 20213001540
+*/
+package newpackage;
+public class DecifragemInvalidaException extends Exception {
+    
+    public DecifragemInvalidaException(){
+        super("Erro ao tentar decifrar a mensagem\n");
+    }
+    
+}
