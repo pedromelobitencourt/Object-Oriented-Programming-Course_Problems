828ecb270b721f70d10ede75524ab812fdc23e98
PedroMeloBitencourt <89912712+pedrinv@users.noreply.github.com>
2022-07-17 11:44:38 -0300

    Create Tridimensional.java

---
+++ CaixaDeBrinquedos/Tridimensional.java
@@ -0,0 +1,8 @@
+/*
+Nome: Pedro Vitor Melo Bitencourt
+Matrícula: 20213001540
+*/
+package principal;
+public abstract class Tridimensional implements Forma {
+    public abstract double obterVolume();
+}
